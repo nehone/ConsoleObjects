16ece9374fa5f520c081e293445c0a42ee402414
Lab314 <Lab314@314-CS01>
2023-01-23 15:59:00 +0200

    Add project files.

---
+++ ConsoleObjects/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace ConsoleObjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 55;
+            int x2 = 0;
+            Point p = new Point(x, x2, "A");
+            int y = 0;
+            int y2 = x2 / 2;
+            Point p2 = new Point(y, y2, "B");
+            for (int i = 0; i < 40; i++)
+            {
+                Console.SetCursorPosition(18, i);
+                Console.WriteLine("|");
+            }
+            for (int i = 0; i < 40; i++)
+            {
+                Console.SetCursorPosition(i, 20);
+                Console.WriteLine("-");
+            }
+
+            p2.Draw();
+            p.Draw();
+            
+            Console.ReadLine();
+        }
+    }
+}
